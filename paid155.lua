863fdbe2e02d28c631340b735fab0ae948e41f19
NightScripts <77138051+NightScripts-API@users.noreply.github.com>
2022-11-20 17:28:22 -0500

    Update paid155.lua

--- paid155.lua
+++ paid155.lua
@@ -1,5 +1,5 @@
 
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/NightScripts-API/rayfield-customized/main/ui.ai'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 
 local Player = game.Players.LocalPlayer
